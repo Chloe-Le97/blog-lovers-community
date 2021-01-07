71e27d3eb2afd5ced00bc66850093808261f07da
Chloe-Le97 <phuonganhkitty365@gmail.com>
2021-01-07 20:45:46 +0200

    deploy to heroku

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const app = require('./app') // the actual Express application
 const http = require('http')
-const config = require('./utils/config')
 const logger = require('./utils/logger')
 
 const server = http.createServer(app)
 
-server.listen(config.PORT, () => {
-  logger.info(`Server running on port ${config.PORT}`)
+config = process.env.PORT || 3000
+
+server.listen(process.env.PORT || 3000, () => {
+  logger.info(`Server running on port ${config}`)
 })
