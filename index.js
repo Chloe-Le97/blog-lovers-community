4a320717217d6e54cdeb496ee7c4521aec358c75
Chloe-Le97 <phuonganhkitty365@gmail.com>
2021-01-07 21:23:38 +0200

    deploy to heroku

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const logger = require('./utils/logger')
 
 const port = process.env.PORT
 
+app.get('*', (request, response) => {
+	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`)
 })
